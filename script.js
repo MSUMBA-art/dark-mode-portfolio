3852d1c03ddda9baa031a5c84b53074bb4f4a156
ALEXANDER <msumba83@gmail.com>
2022-09-08 10:47:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// navigation bar effects on scroll 
+window.addEventListener("scroll", function() {
+    const header = this.document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
