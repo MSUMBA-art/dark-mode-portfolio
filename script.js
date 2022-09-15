37f79513c4129fc6cecdc30d2192a1df72a83fb0
ALEXANDER <msumba83@gmail.com>
2022-09-15 06:57:30 +0200

    done services section

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // navigation bar effects on scroll 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     const header = this.document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//  services section modal 
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
+
+let modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+};
+
+learnMoreBtn.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtn.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+      }) 
+   }) 
 })
