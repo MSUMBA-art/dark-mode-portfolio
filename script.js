a6a9731c63adcaa2a38c6f0b3530f43c746678e2
ALEXANDER <msumba83@gmail.com>
2022-09-21 10:38:22 +0200

    done footer and dark mode

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// navigation bar effects on scroll 
+// navigation bar effects on scroll
 window.addEventListener("scroll", function () {
-    const header = this.document.querySelector('header');
-    header.classList.toggle("sticky", window.scrollY > 0);
+  const header = this.document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-//  services section modal 
+//  services section modal
 const serviceModals = document.querySelectorAll(".service-modal");
 const learnMoreBtn = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtn = document.querySelectorAll(".modal-close-btn");
 
 let modal = function (modalClick) {
-    serviceModals[modalClick].classList.add("active");
+  serviceModals[modalClick].classList.add("active");
 };
 
 learnMoreBtn.forEach((learnMoreBtn, i) => {
-    learnMoreBtn.addEventListener("click", () => {
-        modal(i);
-    });
+  learnMoreBtn.addEventListener("click", () => {
+    modal(i);
+  });
 });
 
 modalCloseBtn.forEach((modalCloseBtn) => {
-    modalCloseBtn.addEventListener("click", () => {
-        serviceModals.forEach((modalView) => {
-            modalView.classList.remove("active");
-        });
+  modalCloseBtn.addEventListener("click", () => {
+    serviceModals.forEach((modalView) => {
+      modalView.classList.remove("active");
     });
+  });
 });
 
 // Portfolio section - Modal
@@ -51,7 +51,7 @@ portfolioCloseBtn.forEach((portfolioCloseBtn) => {
   });
 });
 
-// my clients swiper 
+// my clients swiper
 var swiper = new Swiper(".client-swiper", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -64,4 +64,64 @@ var swiper = new Swiper(".client-swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// website dark/light theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon);
+});
+
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+// scroll to top button
+const scrollToTop = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function () {
+  scrollToTop.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollToTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+// Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+  const section = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
+
+  section.forEach((current) => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let id = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
+    }
+  });
 });
